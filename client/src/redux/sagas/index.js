c5acf8e2969295b31f950f3ae0050c32a17d8ab2
Aiden Kim <aidengookjinkim@gmail.com>
2021-10-04 22:21:02 -0600

    video-chat server init

--- client/src/redux/sagas/index.js
+++
@@ -1,9 +0,0 @@
-import { all, fork } from 'redux-saga/effects';
-import dotenv from 'dotenv';
-import joinSaga from 'joinSaga';
-
-dotenv.config();
-
-export default function* rootSaga() {
-  yield all([fork(joinSaga)]);
-}
